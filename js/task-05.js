bb7bd1cc092e39b1dec31d4b7784560d8c55a7d4
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-02-17 11:46:18 +0200

    Update task-05.js
    task-05 finish

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector('#name-input');
+
+const textOutput = document.querySelector('#name-output');
+
+textInput.addEventListener('input', event => {
+    textOutput.textContent = event.target.value
+
+    if (event.target.value === '') {textOutput.textContent = 'Anonymous'}
+});
+
+console.log(textInput, textOutput);
