68dc5d5badbf2fdde12c70625896e0b8af04ad58
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-02-18 14:22:32 +0200

    Update task-06.js
    task-06 finish

---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+const textInputEl = document.querySelector('#validation-input');
+
+console.log(textInputEl.getAttribute('data-length'), textInputEl);
+
+textInputEl.addEventListener('blur', event => {
+if (event.target.value.length === textInputEl.getAttribute('data-length')) {
+    textInputEl.classList.add('valid')
+    
+if (textInputEl.classList.contains('invalid')) {
+    textInputEl.classList.remove('invalid')
+}
+}
+else {
+if (textInputEl.classList.contains('valid')) {
+    textInputEl.classList.remove('valid')
+}
+    textInputEl.classList.add('invalid')
+}
+});
