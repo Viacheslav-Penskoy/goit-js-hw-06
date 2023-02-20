455bdfc5104ff967a615ee761ebe3535faf9ea4a
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-02-20 10:47:05 +0200

    Update task-08.js
    task-08 finish

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+const messageAlert = 'Все поля должны быть заполнены';
+
+function onFormSubmit(event) {
+  
+    event.preventDefault();
+  
+    const formEl = event.currentTarget.elements;
+
+    const email = formEl.email.value;
+  
+    const password = formEl.password.value.trim();
+
+    const formData = { email, password };
+
+    if (email === '' || password === '') {
+        return alert(messageAlert);
+    }
+
+console.log(formData);
+  
+event.currentTarget.reset();
+}
 
