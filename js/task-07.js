b71575dda0aeb88a2c19f16560f88d6288b14152
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-02-20 10:04:40 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,9 @@
 const inputChangeEl = document.querySelector('input#font-size-control');
+
 console.log(inputChangeEl);
 
 const spanEl = document.querySelector('span#text');
+
 console.log(spanEl);
 
 inputChangeEl.addEventListener('change', event => {
