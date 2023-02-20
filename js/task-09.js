521aa9a51c2e16736666781e9c5e87022ffd6f64
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-02-20 11:35:10 +0200

    Update task-09.js
    task-09 finish

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+console.log(bodyEl);
+
+const buttonChangeEl = document.querySelector('.change-color');
+console.log(buttonChangeEl);
+
+const spanEl = document.querySelector('.color');
+console.log(spanEl);
+
+buttonChangeEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = bodyEl.style.backgroundColor;
+}
