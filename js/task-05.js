96e00826b3a0cc6e97205c6c520a1621f727c16d
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-02-21 11:31:36 +0200

    add trim()

--- js/task-05.js
+++ js/task-05.js
@@ -3,7 +3,8 @@ const textInput = document.querySelector('#name-input');
 const textOutput = document.querySelector('#name-output');
 
 textInput.addEventListener('input', event => {
-    textOutput.textContent = event.target.value
+     
+    textOutput.textContent = event.target.value.trim()
 
     if (event.target.value === '') {textOutput.textContent = 'Anonymous'}
 });
--- js/task-06.js
+++ js/task-06.js
@@ -3,15 +3,17 @@ const textInputEl = document.querySelector('#validation-input');
 console.log(textInputEl.getAttribute('data-length'), textInputEl);
 
 textInputEl.addEventListener('blur', event => {
-if (event.target.value.length == textInputEl.getAttribute('data-length')) {
+
+    if (event.target.value.trim().length == textInputEl.getAttribute('data-length')) {
     textInputEl.classList.add('valid')
     
-if (textInputEl.classList.contains('invalid')) {
+    if (textInputEl.classList.contains('invalid')) {
     textInputEl.classList.remove('invalid')
 }
 }
-else {
-if (textInputEl.classList.contains('valid')) {
+    else {
+    
+    if (textInputEl.classList.contains('valid')) {
     textInputEl.classList.remove('valid')
 }
     textInputEl.classList.add('invalid')
